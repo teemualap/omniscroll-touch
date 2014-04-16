d22965608acbba3fb39b12bd6a7c60e03309ba02
Teemu Alapoikela <teemu.alapoikela@gmail.com>
2014-04-16 20:09:55 +0300

    do not normalize

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var source = 'touchscreen';
 var plugin = function(omniscroll,options) {
 
   var settings = {
-    touchFactor: 2,
+    touchFactor: 1,
     preventDefault: true
   };
 
@@ -17,33 +17,45 @@ var plugin = function(omniscroll,options) {
   var y1 = 0;
   var y2 = 0;
   var delta = 0;
+  var touchDown = false;
 
-  function onEvent(e) {
+  function onMove(e) {
 
     if (settings.preventDefault) {
       e.preventDefault();
     }
 
-    y2 = e.touches[0].pageY;
-
-    delta = (y1 - y2) / Math.abs(y1-y2) * settings.touchFactor || settings.touchFactor;
-
-    omniscroll.consume(delta,source);
+    if (touchDown) {
+      y2 = e.touches[0].clientY;
+      delta = y1 - y2;
+      if (delta > 2 || delta < -2) {
+        y1 = y2;
+        delta *= settings.touchFactor;
+        omniscroll.consume(delta,source);
+      }
+    }
   }
 
-  function onEventStart(e) {
-    y1 = e.touches[0].pageY;
+  function onStart(e) {
+    touchDown = true;
+    y1 = e.touches[0].clientY;
+  }
+
+  function onEnd(e) {
+    touchDown = false;
   }
 
   //exposed interface
   return {
     bind: function(element) {
-      element.addEventListener('touchstart',onEventStart);
-      element.addEventListener('touchmove',onEvent);
+      element.addEventListener('touchstart',onStart);
+      element.addEventListener('touchmove',onMove);
+      element.addEventListener('touchend',onEnd);
     },
     unbind: function(element) {
-      element.removeEventListener('touchstart',onEventStart);
-      element.removeEventListener('touchmove',onEvent);
+      element.removeEventListener('touchstart',onStart);
+      element.removeEventListener('touchmove',onMove);
+      element.removeEventListener('touchend',onEnd);
     }
   }
 
